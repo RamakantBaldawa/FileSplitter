5d0ee257076f6ded48b45b66828931d5c996d2c2
Ramakant baldawa <764462@cognizant.com>
2020-08-28 16:04:28 +0530

    Updated the code to pick file and generate files from and to original directory

--- Splitter.ps1
+++ Splitter.ps1
@@ -8,30 +8,35 @@ param(
 
 #File details
 
+echo $filepath;
+
  $filebasename = (Get-ChildItem $filepath).BaseName;
  $filename = (Get-ChildItem $filepath).Name;
  $extension = (Get-ChildItem $filepath).Extension; 
- 
+ $OriginalDirectory = (Get-ChildItem $filepath).DirectoryName
+echo $filename;
+echo $filebasename;
+echo $extension; 
 
  
  #split file by number of lines
  
  $lowerbound = 0; #Lowerbound Range
  $upperbound = $l; #Uperbound Range
- $linecount = (Get-Content $filename).length;     #Number of lines in file
+ $linecount = (Get-Content $filepath).length;     #Number of lines in file
  $numberoffiles = [Math]::ceiling($linecount/$upperbound);   #Number of files to be generated
- $headerLine = (Get-Content $filename)[0];
+ $headerLine = (Get-Content $filepath)[0];
 
  for($i=0; $i -lt $numberoffiles; $i++)
  {
 $lowerbound++;
 
 if ($header) {
-  @($headerLine)+(get-content $filename)[$lowerbound..$upperbound] | set-content $filebasename'0'$i$extension
+  @($headerLine)+(get-content $filepath)[$lowerbound..$upperbound] | set-content $OriginalDirectory\$filebasename'0'$i$extension
  
 }
 else {
-  (get-content $filename)[$lowerbound..$upperbound] | set-content $filebasename'0'$i$extension
+  (get-content $filepath)[$lowerbound..$upperbound] | set-content $OriginalDirectory\$filebasename'0'$i$extension
 }
  
 $lowerbound=$upperbound;
